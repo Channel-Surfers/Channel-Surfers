290a8254893c353c3829a7005578fcee88500a62
jackjohn7 <branchjohnchristopher@gmail.com>
2024-09-25 15:07:43 -0500

    WIP: adding docs and working on return types

--- src/lib/server/dal/dal.ts
+++ src/lib/server/dal/dal.ts
@@ -1,15 +1,48 @@
-import type { Result } from 'ts-results';
+import { type Result } from 'ts-results';
 import type { Channel } from './schema/channels.sql';
-import type { DbError } from './errors';
+import type { BadOptsError, DbError, ResourceNotFoundError } from './errors';
 
 export type AsyncResult<T, E> = Promise<Result<T, E>>;
 
 // Q: Is there a better place for these type signatures?
-export type GetChannelsOptions = { pageSize: number; page: number };
+/**
+ * Fine-tune what channel data we want to focus on
+ * @property pageSize specifies how many elements are to be fetched
+ * @property page specifies which page to fetch
+ */
+export type GetChannelsOptions = { ownerId: string } & ({ pageSize: number; page: number } | null);
 export type GetChannelsError = DbError;
+/**
+ * Grab a channel by
+ * @property pageSize specifies how many elements are to be fetched
+ * @property page specifies which page to fetch
+ */
 export type GetChannelOptions = { id: string };
-export type GetChannelError = DbError;
+export type GetChannelError = DbError | ResourceNotFoundError | BadOptsError<GetChannelOptions>;
+/**
+ * DAL (Data Access Layer) is a source of truth for what methods exist for retrieving data.
+ * With this interface, we can minimize the amount of database queries explicitly defined
+ * throughout appliication code. Provided some class that `implements DAL`, you can execute
+ * queries as follows (using PostgreSQL as example):
+ *
+ * ```typescript
+ * const dal = new PostgresDAL(db);
+ * const channelsResult = await dal.getChannels();
+ * if (channelsResult.err) {
+ *     // handle your error
+ *     return;
+ * }
+ * // intellisense now knows that our value is of type Channel[]
+ * for (const channel of Channels) {
+ *     console.log(channel);
+ * }
+ * ```
+ */
 export interface DAL {
+    /**
+     * Get a list of channels or an error
+     * @param options allows one to fine-tune the returned results
+     */
     getChannels(options: GetChannelsOptions): AsyncResult<Channel[], GetChannelsError>;
     getChannel(opts: GetChannelOptions): AsyncResult<Channel, GetChannelError>;
 }
--- src/lib/server/dal/errors.ts
+++ src/lib/server/dal/errors.ts
@@ -8,3 +8,7 @@ export const resourceNotFound = (resource: string, message: string) =>
         message,
     }) as const;
 export type ResourceNotFoundError = ReturnType<typeof resourceNotFound>;
+
+export const badOptsError = <T>(message: string, opts: T) =>
+    ({ type: 'BAD_OPTS_ERROR', message, opts }) as const;
+export type BadOptsError<T> = ReturnType<typeof badOptsError<T>>;
