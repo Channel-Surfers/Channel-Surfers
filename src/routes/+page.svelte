99720f30f7550791f9bee690004bee171a1472ac
jackjohn7 <branchjohnchristopher@gmail.com>
2024-09-24 21:30:27 -0500

    added wiki link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,6 @@
 </svelte:head>
 <h1>Welcome to Channel Surfers</h1>
 <p>
-    Visit <a href="https://github.com/Channel-Surfers/Channel-Surfers">the Wiki</a> to read the documentation
+    Visit <a href="https://github.com/Channel-Surfers/Channel-Surfers/wiki">the Wiki</a> to read the
+    documentation
 </p>
