45f0f74fdf854b9bf694d9deca79581ac809d7d7
jackjohn7 <branchjohnchristopher@gmail.com>
2024-09-26 02:21:08 -0500

    created invite

---
+++ src/lib/server/db/invites.sql.ts
@@ -0,0 +1,30 @@
+import { pgTable, primaryKey, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { channel } from './channels.sql';
+import { user } from './users.sql';
+import { role } from './roles.sql';
+
+export const invite = pgTable(
+    'invite',
+    {
+        channelId: uuid('channel_id')
+            .notNull()
+            .references(() => channel.id),
+        userId: uuid('user_id')
+            .notNull()
+            .references(() => user.id),
+        // TODO: When building migration, ensure these expire when the invite table is updated (trigger)
+        createdOn: timestamp('created_on').notNull().defaultNow(),
+        roleId: uuid('role_id')
+            .notNull()
+            .references(() => role.id),
+    },
+    (table) => ({
+        pk: primaryKey({ columns: [table.channelId, table.userId] }),
+    })
+);
+
+/**
+ * Represents an invitation to a user to join a channel
+ */
+export type Invite = typeof invite.$inferSelect;
+export type NewInvite = typeof invite.$inferInsert;
