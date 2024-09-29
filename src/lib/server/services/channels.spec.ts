0208c0b612a89830b27e05e8f99d9f1524c001b0
jackjohn7 <branchjohnchristopher@gmail.com>
2024-09-29 15:51:18 -0500

    removed unused import, using correct expect

--- src/lib/server/services/channels.spec.ts
+++ src/lib/server/services/channels.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'vitest';
 import { getChannelById, getChannels } from './channels';
 import { Effect } from 'effect';
 import { createTestingDb } from '$lib/testing/utils';
@@ -6,48 +6,56 @@ import { channel } from '../db/channels.sql';
 import { user } from '../db/users.sql';
 
 describe('channels suite', () => {
-    it.concurrent('getting channels returns successfully', { timeout: 20000 }, async () => {
-        const { db, generated } = await createTestingDb(async (db) => {
-            const creator = (
-                await db.insert(user).values({ username: 'AwesomeGuy' }).returning()
-            )[0];
-            const createdChannel = (
-                await db
-                    .insert(channel)
-                    .values({ name: 'Channel-Surfers', createdBy: creator.id })
-                    .returning()
-            )[0];
-            return { creator, createdChannel };
-        });
+    it.concurrent(
+        'getting channels returns successfully',
+        { timeout: 20000 },
+        async ({ expect }) => {
+            const { db, generated } = await createTestingDb(async (db) => {
+                const creator = (
+                    await db.insert(user).values({ username: 'AwesomeGuy' }).returning()
+                )[0];
+                const createdChannel = (
+                    await db
+                        .insert(channel)
+                        .values({ name: 'Channel-Surfers', createdBy: creator.id })
+                        .returning()
+                )[0];
+                return { creator, createdChannel };
+            });
 
-        if (!generated) process.exit(1);
+            if (!generated) process.exit(1);
 
-        const { createdChannel } = generated;
+            const { createdChannel } = generated;
 
-        const channels = await Effect.runPromise(getChannels(db));
-        expect(channels.length).toStrictEqual(1);
-        expect(channels[0].name).toStrictEqual(createdChannel.name);
-    });
+            const channels = await Effect.runPromise(getChannels(db));
+            expect(channels.length).toStrictEqual(1);
+            expect(channels[0].name).toStrictEqual(createdChannel.name);
+        }
+    );
 
-    it.concurrent('getting channel by id returns successfully', { timeout: 20000 }, async () => {
-        const { db, generated } = await createTestingDb(async (db) => {
-            const creator = (
-                await db.insert(user).values({ username: 'AwesomeGuy' }).returning()
-            )[0];
-            const createdChannel = (
-                await db
-                    .insert(channel)
-                    .values({ name: 'Channel-Surfers', createdBy: creator.id })
-                    .returning()
-            )[0];
-            return { creator, createdChannel };
-        });
+    it.concurrent(
+        'getting channel by id returns successfully',
+        { timeout: 20000 },
+        async ({ expect }) => {
+            const { db, generated } = await createTestingDb(async (db) => {
+                const creator = (
+                    await db.insert(user).values({ username: 'AwesomeGuy' }).returning()
+                )[0];
+                const createdChannel = (
+                    await db
+                        .insert(channel)
+                        .values({ name: 'Channel-Surfers', createdBy: creator.id })
+                        .returning()
+                )[0];
+                return { creator, createdChannel };
+            });
 
-        if (!generated) process.exit(1);
+            if (!generated) process.exit(1);
 
-        const { createdChannel } = generated;
+            const { createdChannel } = generated;
 
-        const c = await Effect.runPromise(getChannelById(db, createdChannel.id));
-        expect(c?.name).toStrictEqual(createdChannel.name);
-    });
+            const c = await Effect.runPromise(getChannelById(db, createdChannel.id));
+            expect(c?.name).toStrictEqual(createdChannel.name);
+        }
+    );
 });
--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,6 +1,6 @@
 import { getDb } from '$lib/server';
 import { getChannels } from '$lib/server/services/channels';
-import { Effect, Either } from 'effect';
+import { Effect } from 'effect';
 
 export const load = async () => {
     const db = await getDb();
