75170f58711af20e0d8d5270ea89f0c04ce13256
jackjohn7 <branchjohnchristopher@gmail.com>
2024-09-29 23:19:25 -0500

    waiting for loaded state

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }) => {
     await page.goto('/');
+    await page.waitForLoadState();
     expect(page).toHaveTitle('Channel Surfers');
 });
