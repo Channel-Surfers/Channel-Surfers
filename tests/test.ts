c446992373ff4f510ec766f6620a9b5d06c0a9a3
jackjohn7 <branchjohnchristopher@gmail.com>
2024-09-29 23:29:01 -0500

    updated string

--- tests/test.ts
+++ tests/test.ts
@@ -4,5 +4,5 @@ test('home page has expected h1', async ({ page }) => {
     await page.goto('/');
     await page.waitForLoadState('domcontentloaded');
     expect(page).toHaveTitle('');
-    expect(page).toHaveTitle('ChannelSurfers');
+    expect(page).toHaveTitle('Channel Surfers');
 });
