4cd3441a1fffa0a9767a071736ff1be2c432afac
funnyboy-roks <funnyboyroks@gmail.com>
2024-10-02 12:53:16 -0500

    add colour scheme toggle under profile

--- src/lib/components/user/ProfileIcon.svelte
+++ src/lib/components/user/ProfileIcon.svelte
@@ -3,9 +3,15 @@
     import UserIcon from 'lucide-svelte/icons/user';
     import LogOut from 'lucide-svelte/icons/log-out';
     import LogIn from 'lucide-svelte/icons/log-in';
+    import Palette from 'lucide-svelte/icons/palette';
+    import Sun from 'lucide-svelte/icons/sun';
+    import Moon from 'lucide-svelte/icons/moon';
+    import MonitorCog from 'lucide-svelte/icons/monitor-cog';
 
     import * as Avatar from '$lib/shadcn/components/ui/avatar';
     import * as DropdownMenu from '$lib/shadcn/components/ui/dropdown-menu';
+
+    import { resetMode, setMode } from 'mode-watcher';
 
     export let user;
 </script>
@@ -22,6 +28,26 @@
         </Avatar.Root>
     </DropdownMenu.Trigger>
     <DropdownMenu.Content class="w-56">
+        <DropdownMenu.Sub>
+            <DropdownMenu.SubTrigger>
+                <Palette class="mr-2 h-4 w-4" />
+                <span>Color Scheme</span>
+            </DropdownMenu.SubTrigger>
+            <DropdownMenu.SubContent>
+                <DropdownMenu.Item on:click={() => setMode('light')}>
+                    <Sun class="mr-2 h-4 w-4" />
+                    Light
+                </DropdownMenu.Item>
+                <DropdownMenu.Item on:click={() => setMode('dark')}>
+                    <Moon class="mr-2 h-4 w-4" />
+                    Dark
+                </DropdownMenu.Item>
+                <DropdownMenu.Item on:click={() => resetMode()}>
+                    <MonitorCog class="mr-2 h-4 w-4" />
+                    System
+                </DropdownMenu.Item>
+            </DropdownMenu.SubContent>
+        </DropdownMenu.Sub>
         {#if user}
             <DropdownMenu.Item href="/settings">
                 <Settings class="mr-2 h-4 w-4" />
