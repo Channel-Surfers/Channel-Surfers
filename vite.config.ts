2b3bb352c65409d5608e3b94a2bdfb9840a41c59
funnyboy-roks <funnyboyroks@gmail.com>
2024-10-04 10:09:20 -0500

    remove host restrictions

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     plugins: [sveltekit()],
-    server: {
-        host: '127.0.0.1',
-    },
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}'],
         testTimeout: 20000,
