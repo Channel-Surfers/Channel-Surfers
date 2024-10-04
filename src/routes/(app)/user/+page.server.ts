709575ad30edae711f16ac7c1054a5d4302c2cf2
funnyboy-roks <funnyboyroks@gmail.com>
2024-10-04 10:49:28 -0500

    lint

--- src/routes/(app)/user/+page.server.ts
+++ src/routes/(app)/user/+page.server.ts
@@ -1,6 +1,5 @@
-import { fail, redirect } from '@sveltejs/kit';
-import type { Actions, PageServerLoad } from './$types';
-import { signOut } from '$lib/server/auth';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
     if (!event.locals.user) redirect(302, '/signin');
