d3b0ed0a2367b271b1ad2bff7dac3dda1e5c32af
jackjohn7 <branchjohnchristopher@gmail.com>
2024-10-14 10:37:17 -0500

    WIP: getting user posts

--- src/routes/(app)/u/[username]/+page.server.ts
+++ src/routes/(app)/u/[username]/+page.server.ts
@@ -1,7 +1,18 @@
+import { getDb } from '$lib/server';
+import { getPosts } from '$lib/server/services/content';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ params: { username } }) => {
+export const load = (async ({ params: { username }, locals }) => {
+    const db = await getDb();
+    const postQuery = getPosts(db, 0, {
+        type: 'user',
+        username,
+        sort: 'date',
+        filter: 'all',
+        requesterId: locals.user ? locals.user.id : undefined,
+    });
     return {
         username,
+        posts: await postQuery,
     };
 }) satisfies PageServerLoad;
--- src/routes/(app)/u/[username]/+page.svelte
+++ src/routes/(app)/u/[username]/+page.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
+    import InfiniteScroll from '$lib/components/InfiniteScroll.svelte';
+
     export let data;
+
+    let filter = 'all';
+    let sort = 'date';
+    let reverseSort = false;
+    const now = new Date();
+
+    const get_posts = async (page: number) => {
+        const search = new URLSearchParams({
+            page: `${page}`,
+            type: 'user',
+            after: now.toISOString(),
+            username: data.username,
+            sort,
+            filter,
+            reverseSort: `${reverseSort}`,
+        });
+        const res = await fetch(`/api/posts?${search}`);
+
+        if (res.status !== 200) {
+            throw new Error(await res.text());
+        }
+
+        return await res.json();
+    };
 </script>
 
 <h1>Hello {data.username}</h1>
+<InfiniteScroll init_buffer={data.posts} {get_posts} signed_in={!!data.user} />
