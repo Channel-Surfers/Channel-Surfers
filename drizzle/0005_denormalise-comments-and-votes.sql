b38d76d8cf7ef7af2e29e9de3c91e95b37496cf4
funnyboy-roks <funnyboyroks@gmail.com>
2024-10-21 10:28:32 -0500

    update migration to have pk on follows and new denorms

--- drizzle/0005_denormalise-comments-and-votes.sql
+++ drizzle/0005_denormalise-comments-and-votes.sql
@@ -1,24 +1,30 @@
+ALTER TABLE "follow" ADD CONSTRAINT "follow_user_id_follower_id_pk" PRIMARY KEY("user_id","follower_id");--> statement-breakpoint
+ALTER TABLE "channel" ADD COLUMN "subscribers" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "channel" ADD COLUMN "posts" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
 ALTER TABLE "comment" ADD COLUMN "upvotes" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
 ALTER TABLE "comment" ADD COLUMN "downvotes" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "post" ADD COLUMN "comments" integer DEFAULT 0 NOT NULL;
+ALTER TABLE "post" ADD COLUMN "comments" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "followers" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "following" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "follow" ADD CONSTRAINT "follow_follower_id_user_id_fk" FOREIGN KEY ("follower_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "follow" ADD CONSTRAINT "follow_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+-- Comment count denorm
 
 UPDATE post
     SET comments = (
         SELECT count(*) FROM "comment"
             WHERE post_id = post.id
     );
-
-UPDATE "comment"
-    SET upvotes = (
-        SELECT count(*) FROM comment_vote
-            WHERE comment_id = "comment".id AND vote = 'UP'
-    );
-UPDATE "comment"
-    SET downvotes = (
-        SELECT count(*) FROM comment_vote
-            WHERE comment_id = "comment".id AND vote = 'DOWN'
-    );
-
 
 CREATE OR REPLACE FUNCTION insert_comment_fn()
 RETURNS TRIGGER
@@ -45,6 +51,136 @@ BEGIN
     RETURN NEW;
 END;
 $$;
+
+CREATE TRIGGER insert_comment
+AFTER INSERT
+ON "comment"
+FOR EACH ROW
+    EXECUTE FUNCTION insert_comment_fn();
+
+CREATE TRIGGER delete_comment
+AFTER DELETE
+ON "comment"
+FOR EACH ROW
+    EXECUTE FUNCTION delete_comment_fn();
+
+-- Follow count denorm
+
+UPDATE "user"
+    SET followers = (
+        SELECT count(*) FROM "follow"
+            WHERE user_id = "user".id
+    );
+
+UPDATE "user"
+    SET following = (
+        SELECT count(*) FROM "follow"
+            WHERE follower_id = "user".id
+    );
+
+CREATE OR REPLACE FUNCTION insert_follow_fn()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    UPDATE "user"
+        SET followers = "user".followers + 1
+        WHERE "user".id = NEW.user_id;
+    UPDATE "user"
+        SET following = "user".following + 1
+        WHERE "user".id = NEW.follower_id;
+    RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION delete_follow_fn()
+RETURNS TRIGGER 
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    UPDATE "user"
+        SET followers = "user".followers - 1
+        WHERE "user".id = OLD.user_id;
+    UPDATE "user"
+        SET following = "user".following - 1
+        WHERE "user".id = OLD.follower_id;
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER insert_follow
+AFTER INSERT
+ON "follow"
+FOR EACH ROW
+    EXECUTE FUNCTION insert_follow_fn();
+
+CREATE TRIGGER delete_follow
+AFTER DELETE
+ON "follow"
+FOR EACH ROW
+    EXECUTE FUNCTION delete_follow_fn();
+
+-- Subscription count
+
+UPDATE "channel"
+    SET subscribers = (
+        SELECT count(*) FROM "subscription"
+            WHERE channel_id = "channel".id
+    );
+
+CREATE OR REPLACE FUNCTION insert_subscription_fn()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    UPDATE "channel"
+        SET subscribers = "channel".subscribers + 1
+        WHERE "channel".id = NEW.channel_id;
+    RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION delete_subscription_fn()
+RETURNS TRIGGER 
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    UPDATE "channel"
+        SET subscribers = "channel".subscribers - 1
+        WHERE "channel".id = OLD.channel_id;
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER insert_subscription
+AFTER INSERT
+ON "subscription"
+FOR EACH ROW
+    EXECUTE FUNCTION insert_subscription_fn();
+
+CREATE TRIGGER delete_subscription
+AFTER DELETE
+ON "subscription"
+FOR EACH ROW
+    EXECUTE FUNCTION delete_subscription_fn();
+
+-- Comment vote denormalisations
+
+UPDATE "comment"
+    SET upvotes = (
+        SELECT count(*) FROM comment_vote
+            WHERE comment_id = "comment".id AND vote = 'UP'
+    );
+UPDATE "comment"
+    SET downvotes = (
+        SELECT count(*) FROM comment_vote
+            WHERE comment_id = "comment".id AND vote = 'DOWN'
+    );
+
 
 CREATE OR REPLACE FUNCTION update_comment_vote_fn()
 RETURNS TRIGGER 
@@ -111,18 +247,6 @@ BEGIN
 END;
 $$;
 
-CREATE TRIGGER insert_comment
-AFTER INSERT
-ON "comment"
-FOR EACH ROW
-    EXECUTE FUNCTION insert_comment_fn();
-
-CREATE TRIGGER delete_comment
-AFTER DELETE
-ON "comment"
-FOR EACH ROW
-    EXECUTE FUNCTION delete_comment_fn();
-
 CREATE TRIGGER update_comment_vote
 AFTER UPDATE
 ON comment_vote
@@ -140,3 +264,73 @@ AFTER DELETE
 ON comment_vote
 FOR EACH ROW
     EXECUTE FUNCTION delete_comment_vote_fn();
+
+-- Post count denorm
+
+UPDATE "channel"
+    SET posts = (
+        SELECT count(*) FROM "post"
+            WHERE channel_id = "channel".id
+    );
+
+CREATE OR REPLACE FUNCTION insert_post_fn()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    UPDATE "channel"
+        SET posts = "channel".posts + 1
+        WHERE "channel".id = NEW.channel_id;
+    RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION update_post_fn()
+RETURNS TRIGGER 
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    IF NEW.channel_id <> OLD.channel_id THEN
+        UPDATE "channel"
+            SET post = "channel".post - 1
+            WHERE "channel".id = OLD.channel_id;
+        UPDATE "channel"
+            SET post = "channel".post + 1
+            WHERE "channel".id = NEW.channel_id;
+    END IF;
+    RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION delete_post_fn()
+RETURNS TRIGGER 
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    UPDATE "channel"
+        SET posts = "channel".posts - 1
+        WHERE "channel".id = OLD.channel_id;
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER insert_post
+AFTER INSERT
+ON "post"
+FOR EACH ROW
+    EXECUTE FUNCTION insert_post_fn();
+
+CREATE TRIGGER update_post
+AFTER UPDATE
+ON "post"
+FOR EACH ROW
+    EXECUTE FUNCTION update_post_fn();
+
+CREATE TRIGGER delete_post
+AFTER DELETE
+ON "post"
+FOR EACH ROW
+    EXECUTE FUNCTION delete_post_fn();
