963739e8ce22d7037593d4a15b633bbbd41bd8da
funnyboy-roks <funnyboyroks@gmail.com>
2024-10-21 15:29:36 -0500

    default delay 0

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,9 +6,7 @@ import { env } from '$env/dynamic/private';
 
 export const handle: Handle = async ({ event, resolve }) => {
     // Add artificial delay when in dev so we can see what the web requests will be like
-    if (dev) {
-        await sleep(+(env.DELAY ?? 500));
-    }
+    if (dev && env.DELAY) await sleep(+env.DELAY);
     await updateLocals(event);
     return resolve(event);
 };
