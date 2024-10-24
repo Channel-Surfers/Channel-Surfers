609772fb483454994fe6ae50e715daac017a38a0
funnyboy-roks <funnyboyroks@gmail.com>
2024-10-23 19:21:21 -0500

    [wip] create button

--- src/lib/components/sidenav/LeftNav.svelte
+++ src/lib/components/sidenav/LeftNav.svelte
@@ -5,8 +5,12 @@
     import { Home, Settings, Flame } from 'lucide-svelte';
     import Route from './Route.svelte';
     import * as Accordion from '$lib/shadcn/components/ui/accordion';
+    import * as Dialog from '$lib/shadcn/components/ui/dialog';
     import type { UserSubscription } from '$lib/server/services/channels';
     import type { Playlist } from '$lib/server/db/playlists.sql';
+    import { Button, buttonVariants } from '$lib/shadcn/components/ui/button';
+    import { Label } from '$lib/shadcn/components/ui/label';
+    import { Input } from '$lib/shadcn/components/ui/input';
 
     // type signature here is temporary
     export let channels: (Channel & { publicInfo: { displayName: string } | null })[] | null = null;
@@ -37,6 +41,29 @@
                             {:else}
                                 <p class="pl-2">You have no channels</p>
                             {/each}
+                            <Dialog.Root>
+                                <Dialog.Trigger class="w-full flex justify-center">
+                                    <Button class="w-full m-2">Create Channel</Button>
+                                </Dialog.Trigger>
+                                <Dialog.Content class="sm:max-w-[425px]">
+                                    <Dialog.Header>
+                                        <Dialog.Title>Create Channel</Dialog.Title>
+                                    </Dialog.Header>
+                                    <div class="grid gap-4 py-4">
+                                        <div class="grid grid-cols-4 items-center gap-4">
+                                            <Label for="name" class="text-right">Name</Label>
+                                                <Input id="name" value="Pedro Duarte" class="col-span-3" />
+                                        </div>
+                                        <div class="grid grid-cols-4 items-center gap-4">
+                                            <Label for="username" class="text-right">Username</Label>
+                                                <Input id="username" value="@peduarte" class="col-span-3" />
+                                        </div>
+                                    </div>
+                                    <Dialog.Footer>
+                                        <Button type="submit">Save changes</Button>
+                                    </Dialog.Footer>
+                                </Dialog.Content>
+                            </Dialog.Root>
                         {:else}
                             <p class="pl-2">Login to create a channel</p>
                         {/if}
