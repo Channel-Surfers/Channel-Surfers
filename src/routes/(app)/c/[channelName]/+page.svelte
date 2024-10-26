1742592188f46999e58538301ba392fd375e6052
Quentin Quarles <quentavius115@gmail.com>
2024-10-26 01:52:02 -0500

    infinite scroll fixed

--- src/routes/(app)/c/[channelName]/+page.svelte
+++ src/routes/(app)/c/[channelName]/+page.svelte
@@ -27,4 +27,5 @@
     };
 </script>
 
-<InfiniteScroll init_buffer={data.posts} {getPosts} signed_in={!!data.user} />
+<InfiniteScroll init_buffer={data.posts} get_posts={getPosts} signed_in={!!data.user} />
+
