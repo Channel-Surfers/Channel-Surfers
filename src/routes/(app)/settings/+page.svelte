edd0274810e897a9238cf61c7f5edc32115c7cca
Quentin Quarles <quentavius115@gmail.com>
2024-11-13 12:25:56 -0600

    made change to settings page

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -6,15 +6,17 @@
     export let data;
 </script>
 
-<Tabs.Root class="p-2 pt-4">
-    <Tabs.List class="flex grow flex-row">
-        <Tabs.Trigger value="profile" class="flex grow flex-row">Profile</Tabs.Trigger>
-        <Tabs.Trigger value="blocked" class="flex grow flex-row">Blocked</Tabs.Trigger>
-    </Tabs.List>
-    <Tabs.Content value="profile">
-        <Profile userInfo={data.user} />
-    </Tabs.Content>
-    <Tabs.Content value="blocked">
-        <Blocked blocked={data.blockedUser} />
-    </Tabs.Content>
-</Tabs.Root>
+<div class="p-2 pt-4">
+    <Tabs.Root>
+        <Tabs.List class="flex grow flex-row">
+            <Tabs.Trigger value="profile" class="flex grow flex-row">Profile</Tabs.Trigger>
+            <Tabs.Trigger value="blocked" class="flex grow flex-row">Blocked</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="profile">
+            <Profile userInfo={data.user} />
+        </Tabs.Content>
+        <Tabs.Content value="blocked">
+            <Blocked blocked={data.blockedUser} />
+        </Tabs.Content>
+    </Tabs.Root>
+</div>
